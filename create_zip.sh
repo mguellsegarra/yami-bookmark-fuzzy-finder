dab82cf4ea664a16c8b330cfaff0ead0eccf7aef
Marc Güell Segarra <marc@ondori.dev>
2025-04-17 20:13:31 +0200

    create zip script

---
+++ create_zip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create build directory if it doesn't exist
+mkdir -p build
+
+# Get current datetime in the format YYYYMMDD_HHMMSS
+DATETIME=$(date +"%Y%m%d_%H%M%S")
+ZIP_NAME="build/yami_${DATETIME}.zip"
+
+# Create zip file with necessary files
+zip -r "$ZIP_NAME" \
+    manifest.json \
+    content.js \
+    background.js \
+    styles.css \
+    fuse.min.js \
+    icons/ \
+    -x "*.DS_Store" \
+    -x ".*" \
+    -x "__MACOSX/*" \
+    -x "build/*"
+
+echo "Created extension package at: $ZIP_NAME"
